50c5ec297f6c4f4e8560a874fdd9688e8dfcdc3a
shun_neko <shunshunshun112311231123@gmail.comcd>
2023-03-29 20:51:26 +0900

    update

---
+++ atcoder_abc295/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    int h,w;
+    cin >> h >> w;
+    for(int i = 0;i < h; i++){
+        string s;
+        for(int j = 0;j < w; j++){
+            int a;
+            cin >> a;
+            if(a == 0) s.push_back('.');
+            else s.push_back('A' + a-1);
+
+        }
+        cout << s << endl;
+    }
+    return 0;
+}
